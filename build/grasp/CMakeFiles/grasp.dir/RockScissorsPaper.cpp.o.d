84163fc10b57ddbf776a642c743195e4bd345bda
DavidLXu <davidxulixin@gmail.com>
2025-04-23 23:57:12 +0800

    build results

---
+++ build/grasp/CMakeFiles/grasp.dir/RockScissorsPaper.cpp.o.d
@@ -0,0 +1,5 @@
+grasp/CMakeFiles/grasp.dir/RockScissorsPaper.cpp.o: \
+ /data/Downloads/Allegro_Hand_V4/allegro_hand_linux_v4/grasp/RockScissorsPaper.cpp \
+ /usr/include/stdc-predef.h \
+ /data/Downloads/Allegro_Hand_V4/allegro_hand_linux_v4/grasp/rDeviceAllegroHandCANDef.h \
+ /usr/local/include/BHand/BHand.h /usr/local/include/BHand/BHandDef.h
